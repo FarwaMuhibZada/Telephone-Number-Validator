26c9a2981f1fe895d7d3b381c3da21061e0b1ad3
farwaMuhibzada <farwafarid2017@gmail.com>
2024-07-03 13:24:52 +0430

    complete the logic of application in script file.

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const check = document.getElementById('check-btn');
 const clear = document.getElementById('clear-btn');
 const results = document.getElementById('results-div');
 check.addEventListener('click', () => {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
   if (!Input.value) {
     alert('Please provide a phone number');
     return;
+  } else if (regex.test(Input.value)) {
+    results.innerText = `Valid US number: ${Input.value}`;
+  } else {
+    results.innerText = `Invalid US number: ${Input.value}`;
   }
 });
 clear.addEventListener('click', () => {
   results.textContent = '';
   Input.value = '';
 });
+// regex explanitiona
+// optional one with a space (1/s?)?
+// 3digits(\(\d{3}\)|\d{3})
+//optional hyphen or space ([\s-]?)
+// three digit \d{3}
+// optional hyphen or space ([\s-]?)
+// four digit \d{4}
